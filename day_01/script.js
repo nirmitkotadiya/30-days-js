46ec92dccde44310bfc6329c545f9efce6080c56
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-07-14 16:58:08 +0530

    Day 1 Completed
    Day 1 Completed

---
+++ day_01/script.js
@@ -0,0 +1,54 @@
+// Day 1 : Variables, Data Types
+
+// Activity 1 : Variable Declaration
+
+// Task 1
+var number = 19;
+console.log(number);
+
+// Task 2
+let name = "Nirmit";
+console.log(name);
+
+// Activity 2 : Constant Declaration
+// Task 3
+const isAlive = true;
+console.log(isAlive);
+// Activity 3 : Data Types
+// Task 4
+
+// Number
+let num = 32;
+console.log(typeof num);
+
+// String
+let str = "Nirmit";
+console.log(typeof str);
+
+// Boolean
+let bool = true;
+console.log(typeof bool);
+
+// Object
+let obj = { name: "Nirmit", Education: "B.Tech" };
+console.log(typeof obj);
+
+// Array
+let arr = [1, 2, 3, 4, 5];
+console.log(typeof arr);
+
+// Activity 4 : Reassigning Variables
+// Task 5
+
+let age = 19;
+console.log(age);
+age = 20;
+console.log(age);
+
+// Activity 5 : Understanding const
+// Task 6
+
+const PI = 3.14;
+console.log(PI);
+PI = 3.14159;
+console.log(PI);
