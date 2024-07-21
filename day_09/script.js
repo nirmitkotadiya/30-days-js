8ef3468b83eae73d4933f5a7518f654134378ab6
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-07-21 10:00:31 +0530

    Day 9 Completed
    Day 9 Completed

---
+++ day_09/script.js
@@ -0,0 +1,31 @@
+// Day 9 : DOM Manipulation
+
+// Activity 1 : Selecting and Manipulating Elements
+
+// Task 1
+
+// Task 2
+
+// Activity 2 : Creating and Appending Elements
+
+// Task 3
+
+// Task 4
+
+// Activity 3 : Removing Elements
+
+// Task 5
+
+// Task 6
+
+// Activity 4 : Modifying Attributes and Classes
+
+// Task 7
+
+// Task 8
+
+// Activity 5 : Event Handling
+
+// Task 9
+
+// Task 10
